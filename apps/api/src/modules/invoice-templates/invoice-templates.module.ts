78938917df623e9fcee7f6fd2c1d0957a29ed3f9
Jose Bidegain <tu_email@ejemplo.com>
2026-04-02 18:33:29 -0600

    Revert "fix(invoice-templates): importar PlansModule para resolver PlanFeatureGuard dependency"
    This reverts commit 5134f389228d57b5237ad9e2eb7f6ca1ce1bc96c.

--- apps/api/src/modules/invoice-templates/invoice-templates.module.ts
+++ apps/api/src/modules/invoice-templates/invoice-templates.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { InvoiceTemplatesController } from './invoice-templates.controller';
 import { InvoiceTemplatesService } from './invoice-templates.service';
 import { TemplateRenderService } from './template-render.service';
-import { PlansModule } from '../plans/plans.module';
 
 @Module({
-  imports: [PlansModule],
   controllers: [InvoiceTemplatesController],
   providers: [InvoiceTemplatesService, TemplateRenderService],
   exports: [InvoiceTemplatesService, TemplateRenderService],

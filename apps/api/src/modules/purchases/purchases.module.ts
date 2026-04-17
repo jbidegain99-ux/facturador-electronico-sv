520c51f339e187dcdb3c551ec3b76a4a427f3936
Jose Bidegain <tu_email@ejemplo.com>
2026-04-17 16:14:37 -0600

    feat(purchases): register PurchaseReceptionService in module

--- apps/api/src/modules/purchases/purchases.module.ts
+++ apps/api/src/modules/purchases/purchases.module.ts
@@ -2,10 +2,11 @@ import { Module, forwardRef } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AccountingModule } from '../accounting/accounting.module';
 import { PurchasesService } from './services/purchases.service';
+import { PurchaseReceptionService } from './services/purchase-reception.service';
 
 @Module({
   imports: [PrismaModule, forwardRef(() => AccountingModule)],
-  providers: [PurchasesService],
-  exports: [PurchasesService],
+  providers: [PurchasesService, PurchaseReceptionService],
+  exports: [PurchasesService, PurchaseReceptionService],
 })
 export class PurchasesModule {}
